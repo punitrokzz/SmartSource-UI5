b8c030c07431ae6d1dfd653f81ba389b22f5cbe1
Wx0826 <wx862149260@gmail.com>
2023-02-01 16:50:38 +0100

    -

--- WebContent/webapp/controller/formatter.js
+++ WebContent/webapp/controller/formatter.js
@@ -1,11 +1,10 @@
 sap.ui.define([
 	// "sap/ui/core/format/NumberFormat",
-	// "./utilities"
 ], function () {
 	"use strict";
 
 	return {
-		textFormatter: function (str) {
+		textFormat: function (str) {
 			try {
 				var words = str.toLowerCase().split(" ");
 				for (var i in words) {
@@ -13,25 +12,18 @@ sap.ui.define([
 				}
 				return words.join(" ");
 			} catch (error) {
-				console.log(error)
 				return str
 			}
 		},
-		dateFormatter: function (str) {
-			// console.log(str)
-			var date = new Date(str);
-			// print the date
-			// console.log(date);
-			// console.log(date.toGMTString());
 
-			// var d = Date.parse(str);
-			// print the date
-			// console.log(d);
-			// var words = str.toLowerCase().split(" ");
-			// for (var i in words) {
-			// 	words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-			// }
-			return date.toDateString();
+		dateFormat: function (str) {
+			try {
+				var date = new Date(str);
+				var dateString = date.toDateString();
+				return dateString;
+			} catch (error) {
+				return str
+			}
 		}
 	};
 });
